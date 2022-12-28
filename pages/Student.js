0d5b90c26528aff9843b0b7e61f52de689524544
Ismail Hossain <mdismail645221@gmail.com>
2022-12-28 13:26:17 +0600

    student list table added

--- pages/Student.js
+++ pages/Student.js
@@ -6,6 +6,8 @@ import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import { red } from '@mui/material/colors';
 import AddModal from '../Components/AddModal/AddModal';
 import { Button } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowRightSharpIcon from '@mui/icons-material/ArrowRightSharp';
 
 const Student = () => {
     const [open, setOpen] = React.useState(false);
@@ -13,11 +15,13 @@ const Student = () => {
     const handleClose = () => setOpen(false);
 
     const [students, setStudent] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch("http://localhost:5000/students")
-        .then(res=> res.json())
-        .then(data=> setStudent(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setStudent(data))
+    }, [])
+
+    console.log(students)
 
 
     return (
@@ -26,38 +30,77 @@ const Student = () => {
             {/* <h1>THIS IS STUDENTS</h1> */}
 
 
-        {/* list & added  */}
+            {/* list & added  */}
 
-        <div className='mt-10' style={{
-            borderBottom: "1px solid #ddd"
-        }}>
-            <ul className='flex justify-between items-center'>
-                <li className='px-5' style={{
-                    border: "2px solid #ddd",
-                    borderBottom: "none"
-                }}><ViewListIcon/>List</li>
-                <li onClick={handleOpen}  style={{
-                    background: "#eee",
-                    padding: "4px 8px",
-                    borderRadius: "15px",
-                    border: "2px solid #ddd",
-                    cursor: "pointer"
-                }}><ControlPointIcon className='mr-2' />Add new student</li>
-                {/* <Button >Open modal</Button> */}
-            </ul>
-        </div>
+            <div className='mt-10' style={{
+                borderBottom: "1px solid #ddd"
+            }}>
+                <ul className='flex justify-between items-center'>
+                    <li className='px-5' style={{
+                        border: "2px solid #ddd",
+                        borderBottom: "none"
+                    }}><ViewListIcon />List</li>
+                    <li onClick={handleOpen} style={{
+                        background: "#eee",
+                        padding: "4px 8px",
+                        borderRadius: "15px",
+                        border: "2px solid #ddd",
+                        cursor: "pointer"
+                    }}><ControlPointIcon className='mr-2' />Add new student</li>
+                    {/* <Button >Open modal</Button> */}
+                </ul>
+            </div>
 
 
-            <section className='mt-20'>
-                <div className='grid gap-5 grid-cols-1 md:grid-cols-3'>
-                    {students.map(stu=> <Stu key={stu._id} stu={stu}></Stu> )}
-                </div>
-            </section>
+            <div className="overflow-x-auto mt-10">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead className='bg-[#eee]'>
+                        <tr>
+                            {/* <th></th> */}
+                            <th>Name</th>
+                            <th>Roll</th>
+                            <th>INFO</th>
+                            <th className='text-center'>ACATION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-            <AddModal 
+                        {
+                            students.map((user, idx) => <tr key={user._id} className={`${idx==1 && "bg-[#eee]"} ${idx==3 && "bg-[#eee]"}`}>
+                                {/* <th className='text-center'>{idx + 1}</th> */}
+                                <td className='text-center'>{user?.name}</td>
+                                <td className='text-center'>{user?.roll}</td>
+                                <td className='flex justify-center mt-5'>
+                                    <div>
+                                        <p><strong>Class: </strong>{user.class}</p>
+                                        <p><strong>Email : </strong>{user?.email ? user.email : " [Not provided]"}</p>
+                                        <p><strong>Contact no: </strong>{user.phone}</p>
+                                        <p><strong>Gender: </strong>{user.Gender}</p>
+                                        <p><strong>Date of birth: </strong>{user.dataOfBirth}</p>
+                                        <p><strong>Blood Group: </strong>{user.bloodGroup}</p>
+                                        <p><strong>Reg. no: </strong>{user._id}</p>
+                                        <p><strong>Father name: </strong>{user.fatherName}</p>
+                                        <p><strong>Mother name: </strong>{user.motherName}</p>
+                                    </div>
+                                </td>
+                                <td className='text-center cursor-pointer'>
+                                    <div>
+                                        <span><SettingsIcon/></span>
+                                        <span><ArrowRightSharpIcon/></span>
+                                    </div>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+
+            <AddModal
                 handleOpen={handleOpen}
-                 handleClose={handleClose}
-                 open={open}
+                handleClose={handleClose}
+                open={open}
             ></AddModal>
         </div>
     );
